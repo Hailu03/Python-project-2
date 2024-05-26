fea32cdab424b7d7dc2a0a73145bd9a2b19b3dce
Dzit Cute <10623066@student.vgu.edu.vn>
2024-05-27 00:28:19 +0700

    nganupdate

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,7 @@ def load_lottiefile(file_path:str):
         return json.load(f)
 lottie_sleep = load_lottiefile("imgs/16.json")
 lottie_octo = load_lottiefile("imgs/octobus.json")
+lottie_chart = load_lottiefile("imgs/chart.json")
 
 df = pd.read_csv('health.csv')
 
@@ -114,8 +115,8 @@ page_bg_img = f"""
         color: black;
         text-align: justify;
         text-indent: 50px;
-        margin: 10px 40px;
-        padding: 30px 80px;
+        margin: 10px 20px;
+        padding: 30px 40px;
     }}
     .home-paragraph1 .home-paragraph-title {{
         font-family: 'Times', sans-serif;
@@ -131,7 +132,7 @@ page_bg_img = f"""
         color: black;
         text-align: left;
         text-indent: 50px;
-        padding: 10px 80px;
+        padding: 10px 40px;
     }}
     .home-paragraph2 .home-paragraph-title {{
         font-family: 'Times', sans-serif;
@@ -153,7 +154,7 @@ page_bg_img = f"""
         color: black;
         text-align: left;
         text-indent: 50px;
-        padding: 10px 80px;
+        padding: 30px 40px;
         margin-bottom: 100px;
     }}
     .home-paragraph3 .home-paragraph-title {{
@@ -163,23 +164,29 @@ page_bg_img = f"""
         color: black;
         text-align: left;
     }}
-
+    .home-paragraph4 {{
+         font-family: 'Nunito', sans-serif;
+        font-size: 18px;
+        color: black;
+        text-align: left;
+        text-indent: 50px;
+        padding: 5px 35px;
+    }}
     .element-container {{
         display: flex;
         justify-content: center;
         align-items: center;
     }}
-
-        
     [data-testid="element-container"] iframe {{
         position: absolute;
         top: 180px;
         left: 0;
         bottom: 0px;
         right: 50px;
-        filter: brightness(1.2); 
-        
+        filter: brightness(1.2);   
     }}
+
+    hr {{ border: 0.5px solid #06B2B8; margin-top: 15px; margin-bottom: 5px; }}
 
 </style>
 """
@@ -199,17 +206,115 @@ def home():
             <div class='new-page'>
             <div class='home-paragraph1'>
                 <h2 class='home-paragraph-title'>Introduction</h2>
-                <p>This document, produced by Group 4 under the supervision of Dr. Do Duc Tan, investigates the relationship between sleep health and lifestyle. Using the statistical software R, our group examined a detailed dataset (consisting of 13 columns and 375 rows) to reveal insights concerning sleep quality, stress levels, sleep duration, as well as their correlations with gender, age, and occupation.</p>
-                <p>The report features visually appealing graphs created using R Studio, along with thorough explanations, offering a comprehensive interpretation of the data's significance for sleep health and lifestyle. Exercises from the book "R for Data Science" are included to strengthen understanding and provide practical examples.</p>
-                <p>This report highlights the hard work and knowledge of Group 4 under Dr. Tan Duc Do's supervision, with the goal of deepening our comprehension of sleep health and lifestyle to promote well-being and sustain a healthy lifestyle. </p>
+                <hr>
+                <p>This document, produced by Group 4 under the supervision of Dr. Do Duc Tan, investigates the relationship between sleep health and lifestyle. Using the statistical software R, our group examined a detailed dataset (consisting of 13 columns and 375 rows) to reveal insights concerning sleep quality, stress levels, sleep duration, as well as their correlations with gender, age, and occupation.
+                The report features visually appealing graphs created using R Studio, along with thorough explanations, offering a comprehensive interpretation of the data's significance for sleep health and lifestyle. Exercises from the book "R for Data Science" are included to strengthen understanding and provide practical examples.
+                This report highlights the hard work and knowledge of Group 4 under Dr. Tan Duc Do's supervision, with the goal of deepening our comprehension of sleep health and lifestyle to promote well-being and sustain a healthy lifestyle. </p>
             </div>
                     </div>
         """, unsafe_allow_html=True)
 
     # Display Lottie animation in the second column
     with col2:
-        st_lottie(lottie_sleep, key="flex_lottie", width=300, height=400)
+        st_lottie(lottie_sleep, key="flex_lottie_sleep", width=400, height=250)
 
+    st.markdown(f"""
+                <style>
+                    .title2 {{
+                        margin-left: 50px;
+                        margin-right: 50px;
+                    }}
+                
+                </style>
+    """, unsafe_allow_html=True)
+    st.markdown("<div class='title2'><h3 class='home-paragraph-title'>Delving into Code and Visuals: A Journey through Analysis and Visualization</h3><hr></div>", unsafe_allow_html=True)
+    col1, col2 = st.columns([1, 3], gap = "small")
+    with col1:
+        st_lottie(lottie_octo, key="flex_lottie_octo", width=400, height=400)
+    with col2:
+        st.markdown(f"""
+            <div class='new-page'>
+                <div class='home-paragraph2'>
+                    <div style="display: flex; flex-direction: row; align-items: flex-start;">
+                    <div style="flex: 1; padding-right: 10px;">
+                <dl>
+                    <dt> - The Sleep Quality Sweet Spot:</dt>
+                    <dd>Objective: Identify the optimal sleep duration for the best sleep quality.</dd>
+                    <dd>Method: Create scatter plots to visualize the relationship between sleep duration and quality.</dd>
+                    <dt> - Occupation and Sleep:</dt>
+                    <dd>Objective: Compare how different professions affect sleep patterns.</dd>
+                    <dd>Method: Analyze average sleep duration and quality across various occupations.</dd>
+                    <dt> - Physical Activity and Sleep Health:</dt>
+                    <dd>Objective: Correlate physical activity levels with sleep quality and duration.</dd>
+                    <dd>Method: Use scatter plots and correlation coefficients to assess the relationship.</dd>
+                    <dt> - Stress and Sleep:</dt>
+                    <dd>Objective: Examine the impact of stress on sleep duration and quality.</dd>
+                    <dd>Method: Use linear regression analysis to explore this relationship.</dd>
+                    <dt> - Body Mass Index (BMI) and Sleep:</dt>
+                    <dd>Objective: Explore the connection between BMI categories and sleep health.</dd>
+                    <dd>Method: Use scatter plots and statistical tests to analyze the relationship.</dd>
+                    <dt> - Physiology and Sleep:</dt>
+                    <dd>Objective: Analyze the correlation between blood pressure, heart rate, and sleep disturbances.</dd>
+                    <dd>Method: Use appropriate statistical tests to investigate the relationship.</dd>
+                    <dt> - Sleep Disorders:</dt>
+                    <dd>Objective: Analyze the prevalence of sleep disorders and identify common traits among those affected.</dd>
+                    <dd>Method: Use descriptive statistics to assess the occurrence of sleep disorders.</dd>
+                </dl>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        """, unsafe_allow_html=True)
+    st.markdown(f"""
+                <style>
+                    .title3 {{
+                        margin-left: 50px;
+                        margin-right: 50px;
+                    }}
+                
+                </style>
+    """, unsafe_allow_html=True)
+    st.markdown("<div class='title3'><h4 class='home-paragraph-title'>Graphical Analysis: We've crafted 13 graphs, covering 10 different categories:</h4><hr></div>", unsafe_allow_html=True)
+    col1, col2 = st.columns([3, 1], gap = "small")
+    with col1:
+        st.markdown(f"""
+            <div class='new-page'>
+                <div class='home-paragraph4'>
+                    <div style="display: flex; flex-direction: row; align-items: flex-start;">
+                    <div style="flex: 1; padding-right: 10px;">
+                <dl>
+                    <dt>Box plot:</dt> 
+                    <dd>BMI Categories and Their Impact on Heart Rate</dd>
+                    <dt>Bar chart:</dt>
+                    <dd>Relationship between Physical Activity Level, Heart Rate and Gender</dd>
+                    <dd>Distribution of BMI Categories by Gender</dd> 
+                    <dt>line chart:</dt>
+                    <dd>Gender-Based Trends in Daily Step Counts Across Age Groups</dd>
+                    <dt>Pie chart:</dt>
+                    <dd>Relationship Between Sleep Quality and Heart Rate Among Surveyed Individuals</dd>
+                    <dt>Density plot:</dt>
+                    <dd> Relationship between Gender, Occupation, and Sleep Duration</dd>
+                    <dd> Density of Sleep Duration by Gender and Stress Level</dd>
+                    <dt>Ridgeline density plot:</dt>
+                    <dd>Gender-Specific Stress Level Trends Across Age Categories</dd>
+                    <dt>heatmap</dt>
+                    <dd>Heatmap of Stress Level vs Sleep Disorder</dd>
+                    <dd>Comparative Analysis of Body Mass Index Categories Across Professional Sectors</dd>
+                    <dt>Violin plot:</dt>
+                    <dd>Daily Steps Distribution by Gender: A Look at the Violin Plot</dd>
+                    <dt>Bubble chart:</dt>
+                    <dd>Analysis of Stress Levels Across Different Occupations and Age Groups</dd>
+                    <dt>Grouped bar plot with facets:</dt>
+                    <dd>Trends of Quality of Sleep by Gender</dd>
+                </dl>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        """, unsafe_allow_html=True)
+    with col2:
+        st_lottie(lottie_chart, key="flex_lottie_chart", width=300, height=300)
+        
   # Add paragraph 
     st.markdown(f"""
         <style>
@@ -217,25 +322,9 @@ def home():
             .home-paragraph1, .home-paragraph2 {{ margin-bottom: 10px; }}
             .home-paragraph-title {{ margin-bottom: 5px; }}
             p {{ margin-bottom: 5px; }}
-            hr {{ border: 0.5px solid black; margin-top: 35px; margin-bottom: 5px; }}
+            
         </style>
         <div class='new-page'>
-            <hr>
-            <div class='home-paragraph2'>
-                <h3 class='home-paragraph-title'>What do we want to explore from it?</h3>
-                <div style="display: flex; flex-direction: row; align-items: flex-start;">
-                    <div style="flex: 1; padding-right: 10px;">
-                        <p>- Sleep Duration vs. Quality: Explore if there's an optimal sleep duration for the best quality. Visualize this relationship using scatter plots.</p>
-                        <p>- Occupation and Sleep: Analyze how different professions affect sleep patterns by comparing average sleep duration and quality.</p>
-                        <p>- Physical Activity and Sleep Health: Investigate the correlation between physical activity levels and sleep quality/duration.</p>
-                        <p>- Stress and Sleep: Examine how stress impacts sleep duration and quality using linear regression analysis.</p>
-                        <p>- Body Mass Index (BMI) and Sleep: Explore the connection between BMI categories and sleep health, both visually and statistically.</p>
-                        <p>- Physiology and Sleep: Analyze whether blood pressure and heart rate correlate with sleep disturbances using suitable statistical tests. </p>
-                        <p>- Sleep Disorders: Analyze sleep disorder prevalence and identify shared traits among those affected.</p>
-                    </div>
-                </div>
-            </div>
-                <hr>
                 <div class='home-paragraph3'>
                     <h4 class='home-paragraph-title'>Key Features of the Dataset</h4>
                 <div style="display: flex; flex-direction: row; align-items: flex-start;">
@@ -288,7 +377,7 @@ def datasetPage():
     # Define the different pages
     with st.container():
         # Section 1: 
-        st.markdown("<hr style='border:0.5px solid black'>", unsafe_allow_html=True)
+        st.markdown("<hr style='border:0.5px solid #06B2B8'>", unsafe_allow_html=True)
         col1, col2 = st.columns([2, 1])
         with col1:
             st.markdown(
@@ -321,7 +410,7 @@ def datasetPage():
         st.markdown("<div class='divider'></div>", unsafe_allow_html=True)  
 
         # Section 2: Why did we choose this data set? 
-        st.markdown("<hr style='border:0.5px solid black'>", unsafe_allow_html=True)
+        st.markdown("<hr style='border:0.5px solid #06B2B8'>", unsafe_allow_html=True)
         st.markdown("<div class='datasetPage home-paragraph-title'><h3>Why did we choose this data set?</h3></div>", unsafe_allow_html=True)
         
         col1, col2 = st.columns(2) 
@@ -343,7 +432,7 @@ def datasetPage():
         st.markdown("<div class='divider'></div>", unsafe_allow_html=True)  
 
         # Section 3: What do we want to explore from it?
-        st.markdown("<hr style='border:0.5px solid black'>", unsafe_allow_html=True)
+        st.markdown("<hr style='border:0.5px solid #06B2B8'>", unsafe_allow_html=True)
         st.markdown("<div class='datasetPage home-paragraph-title'><h3>What do we want to explore from it?</h3></div>", unsafe_allow_html=True)
         col1, col2 = st.columns([2, 1])
         with col1:
@@ -370,7 +459,7 @@ def datasetPage():
         st.markdown("<div class='divider'></div>", unsafe_allow_html=True)  
 
         # Section 4: Dataset 
-        st.markdown("<hr style='border:0.5px solid black'>", unsafe_allow_html=True)
+        st.markdown("<hr style='border:0.5px solid #06B2B8'>", unsafe_allow_html=True)
         st.markdown("<div class='datasetPage home-paragraph-title'><h3>Dataset</h3></div>", unsafe_allow_html=True)
         try:
             # Filters
